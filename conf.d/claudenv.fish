037819e54c981fefeb33d6f2d548efc2a9ff0c55
Manuel Escalante <maescalantehe@gmail.com>
2026-01-28 16:11:04 +0100

    feat: update CLAUDE_CONFIG_DIR setting to prevent overwriting existing configuration

--- conf.d/claudenv.fish
+++ conf.d/claudenv.fish
@@ -5,6 +5,9 @@ set -l _ce_current_file $_ce_base/.current
 if test -f $_ce_current_file
     set -l _ce_saved (cat $_ce_current_file | string trim)
     if test -d $_ce_base/$_ce_saved
-        set -gx CLAUDE_CONFIG_DIR $_ce_base/$_ce_saved
+        # Only set if not already set (universal persists across sessions)
+        if not set -q CLAUDE_CONFIG_DIR
+            set -Ux CLAUDE_CONFIG_DIR $_ce_base/$_ce_saved
+        end
     end
 end
